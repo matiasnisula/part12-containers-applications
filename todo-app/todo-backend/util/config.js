d75ddffcb1db5ab0c8c36a8c0f7b203eaff9ca54
Matias Nisula <matiasnisula81@gmail.com>
2022-12-23 20:00:28 +0200

    12.9

--- todo-app/todo-backend/util/config.js
+++ todo-app/todo-backend/util/config.js
@@ -1,6 +1,7 @@
 const MONGO_URL =
   process.env.MONGO_URL ||
-  "MONGO_URL=mongodb://the_username:the_password@localhost:3456/the_database";
+  "MONGO_URL=mongodb://the_username:the_password@localhost:3456/the_database" ||
+  undefined;
 const REDIS_URL = process.env.REDIS_URL || undefined;
 
 module.exports = {
