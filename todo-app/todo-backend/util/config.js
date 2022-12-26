9e306ed84e58d7aa3c57e1a12d6fcc74a71caf7c
Matias Nisula <matiasnisula81@gmail.com>
2022-12-26 16:02:08 +0200

    12.16, add backend url to frontend docker-compose.dev.yml file

--- todo-app/todo-backend/util/config.js
+++ todo-app/todo-backend/util/config.js
@@ -1,7 +1,4 @@
-const MONGO_URL =
-  process.env.MONGO_URL ||
-  "MONGO_URL=mongodb://the_username:the_password@localhost:3456/the_database" ||
-  undefined;
+const MONGO_URL = process.env.MONGO_URL || undefined;
 const REDIS_URL = process.env.REDIS_URL || undefined;
 
 module.exports = {
